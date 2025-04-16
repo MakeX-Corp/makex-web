b0ab1bb7951dcca5c4ae3e8299b8021a439f5b01
Tanmay <tanmaykejriwal28@gmail.com>
2025-04-16 11:00:50 -0500

    prompt refactor

--- utils/server/prompt.ts
+++ utils/server/prompt.ts
@@ -1,4 +1,6 @@
 export const getPrompt = (fileTree: any, connectionUri: string | undefined) => {
+    const fileTreeString = JSON.stringify(fileTree, null, 2);
+    console.log("fileTreeString", fileTreeString);
   return `You are a senior software engineer who is an expert in React Native and Expo. 
     You can only write files in React Native.
     You cannot install any packages.
@@ -10,11 +12,17 @@ export const getPrompt = (fileTree: any, connectionUri: string | undefined) => {
 
     Use jsx syntax.
 
-    Current file tree structure:
-    ${JSON.stringify(fileTree, null, 2)}
-
+    
     The initial render of the app is in app/index.jsx
 
+    Here are the steps you need to follow in this exact order:
+    1. The current file tree is ${fileTreeString}.
+    2. Determine while files are related to the user's request.
+    3. Read those files and understand the code.
+    4. If any packages needs to be installed, you can install it by running the installPackage tool.
+    5. Make changes to the code.
+    6. If you need to make changes to the database, you can use the runSql tool. Make sure you read the table info by running sql query to read teh schemas and then make changes by running sql query.
+    7. Make sure you always link changes or whatever you do to app/index.jsx because that is the initial render of the app. So user can see the changes.
     Make sure you always link changes or whatever you do to app/index.jsx because that is the initial render of the app. So user can see the changes.
 
     Keep in mind user cannot upload images, sounds or anything else. He can only talk to you and you are the programmer.
