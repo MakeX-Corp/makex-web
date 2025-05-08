48f64326a9588604edba0ef5877d2b2bedd741c2
tkejr <tanmaykejriwal28@gmail.com>
2025-05-08 09:41:14 -0500

    middleware fix

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -45,6 +45,10 @@ export async function updateSession(request: NextRequest) {
     !request.nextUrl.pathname.startsWith('/login') &&
     !request.nextUrl.pathname.startsWith('/auth/callback') &&
     !request.nextUrl.pathname.startsWith('/signup') &&
+    !request.nextUrl.pathname.startsWith('/support') &&
+    !request.nextUrl.pathname.startsWith('/privacy') &&
+    !request.nextUrl.pathname.startsWith('/refund') &&
+    !request.nextUrl.pathname.startsWith('/terms') &&
     request.nextUrl.pathname !== '/'
   ) {
     // no user, potentially respond by redirecting the user to the login page
