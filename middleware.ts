9964e191177ba58daab6c3a4fd00967faf9ea1c5
Alberto Gaucin <albertogaucin.ag@gmail.com>
2025-07-30 22:28:43 -0500

    middleware

--- middleware.ts
+++ middleware.ts
@@ -14,6 +14,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|api/revenue-cat|api/webhooks/paddle|api/apple|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
