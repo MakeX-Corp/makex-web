4d1b509b902216e21656cdbc005c379c941eb1cb
tkejr <tanmaykejriwal28@gmail.com>
2025-08-02 21:02:29 -0500

    fixed

--- supabase/migrations/20250114000000_remove_content_and_sandbox_id.sql
+++ supabase/migrations/20250114000000_remove_content_and_sandbox_id.sql
@@ -6,5 +6,10 @@ BEGIN
   END IF;
 END $$;
 
--- Remove sandbox_id column from user_apps table  
-ALTER TABLE public.user_apps DROP COLUMN IF EXISTS sandbox_id; 
+-- Remove sandbox_id column from user_apps table (if table exists)
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'user_apps' AND table_schema = 'public') THEN
+    ALTER TABLE public.user_apps DROP COLUMN IF EXISTS sandbox_id;
+  END IF;
+END $$; 
