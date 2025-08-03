79f520a6f67c883a4f0d103d596dd14811a2db56
tkejr <tanmaykejriwal28@gmail.com>
2025-08-02 21:06:01 -0500

    fix2

--- supabase/migrations/20250114000001_rename_app_chat_history_to_chat_history.sql
+++ supabase/migrations/20250114000001_rename_app_chat_history_to_chat_history.sql
@@ -1,2 +1,9 @@
--- Remove content column from chat_history table (after rename)
-ALTER TABLE public.chat_history DROP COLUMN IF EXISTS content; 
+-- Remove content column from chat_history table (or app_chat_history if rename didn't work)
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'chat_history' AND table_schema = 'public') THEN
+    ALTER TABLE public.chat_history DROP COLUMN IF EXISTS content;
+  ELSIF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'app_chat_history' AND table_schema = 'public') THEN
+    ALTER TABLE public.app_chat_history DROP COLUMN IF EXISTS content;
+  END IF;
+END $$; 
